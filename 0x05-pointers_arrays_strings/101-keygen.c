194e66a595cf8508cd422cdaf491a10bd74d06af
sammie05 <snweke98@gmail.com>
2023-02-28 06:16:35 -0800

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -46,7 +46,7 @@ int main(void)
 			}
 		}
 
-		for (index = 0; passwaord[index]; index++)
+		for (index = 0; password[index]; index++)
 		
 		{
 			if (password[index] >= (33 + diff_half2))	
@@ -58,6 +58,7 @@ int main(void)
 
 		}
 	}
+
 	printf("%s", password);
 	return (0);
 }
